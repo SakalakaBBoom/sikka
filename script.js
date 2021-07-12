623592983de004922ad61b64a2403d730c676e96
SakalakaBBoom <86681676+SakalakaBBoom@users.noreply.github.com>
2021-07-12 13:38:06 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-let clickBtn = document.querySelector('button');
-clickBtn.addEventListener('click', showMsg);
-function showMsg(){
-    alert("Confirm before proceed, it seems unethical elements has access to you. ");
-}
