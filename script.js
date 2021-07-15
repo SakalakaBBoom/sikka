ec247f9ca4009caccb5cd98bae90c2f7c427e369
SakalakaBBoom <86681676+SakalakaBBoom@users.noreply.github.com>
2021-07-15 22:46:56 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-let clickBtn = document.querySelector('button');
-clickBtn.addEventListener('click', showMsg);
-function showMsg(){
-    alert("Thank You! Your response has been recorded. We'll try to reach you soon");
-}
